64b03bdba30e7d13f8e1e353520b5c60b391c894
arunwongdotw <arunwongdotw@gmail.com>
2015-10-29 11:11:11 +0700

    สร้าง CalCom คำนวณค่าตอบแทน

---
+++ src/CalCom.java
@@ -0,0 +1,25 @@
+
+public class CalCom {
+	String name;
+	double salary;
+	double sales;
+	double Commission;
+	double compensation;
+	
+	public CalCom(double s,double sa) {
+		this.salary = s;
+		this.sales = sa;
+	}
+	
+	public double getCom(){
+		if(sales > 50000){
+			Commission = sales*0.03;
+		}else if(sales > 25000){
+			Commission = sales*0.02;
+		}else{
+			Commission = sales*0.01;
+		}
+		compensation = salary+Commission;
+		return compensation;
+	}
+}
